bbc592a5af8e14829914deba03680a2b464c6fd9
Aleksandar Prackovski <aleksandar.prackovski@gmail.com>
2022-03-15 20:50:43 +0100

    add basic routes for tasks

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// show all tasks
+Route::get('/task', [TaskController::class, 'index']);
+// Show form for creating tasks
+Route::get('/task/create', [TaskController::class, 'create']);
+// show single task
+Route::get('/task/{task}', [UserController::class, 'show']);
+// store task into db
+Route::post('/task', [UserController::class, 'store']);
+// show form for editing tasks
+Route::get('/task/{task}/edit', [UserController::class, 'edit']);
+// update task into DB
+Route::put('/task/{task}', [UserController::class, 'update']);
+// delete task
+Route::delete('/task/{task}', [UserController::class, 'delete']);
+
